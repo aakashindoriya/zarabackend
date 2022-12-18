a8f1fb27cb86c29a3e7222abd5bd9e3e50195e4f
aakash <37771235+aakashindoriya@users.noreply.github.com>
2022-12-18 13:52:19 +0530

    a

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 let mongoose=require("mongoose")
 mongoose.set('strictQuery', false);
 let connect= async ()=>{
-    return mongoose.connect(process.env.DB_url)
+    return mongoose.connect("mongodb+srv://aakashindoriya:aakash123@cluster0.tbtucsa.mongodb.net/?retryWrites=true&w=majority")
 }
 
 module.exports=connect
