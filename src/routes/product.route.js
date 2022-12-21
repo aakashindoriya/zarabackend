002bc8d7ca92310665dda9366cbff0437964cbcf
aakash <37771235+aakashindoriya@users.noreply.github.com>
2022-12-21 14:08:46 +0530

    sorting done

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -3,16 +3,28 @@ const app=express.Router();
 const Product=require("../module/product.module")
 
 app.get('/', async (req, res) => {
-    let { q, category, price, color, limit, page } = req.query;
+    let { q, category, price, color, limit, page,sort,order } = req.query;
+    
     let query = {};
+    let sorting={};
     if (category) query.category = category;
     if (color) query.color = { $regex: new RegExp(color, 'i') };
     if (q) query.title = { $regex: new RegExp(q, 'i') };
     if (price) query.price = { $lte: price };
     if(!limit) limit = 12;
     if(!page) page = 1;
+    if(sort&&order){
+        if(order=="asc"){
+            order=1
+        }else if(order=="desc"){
+            order=-1
+        }
+        sorting[sort]=order
+    }
+
     try {
-        const products = await Product.find(query).limit(limit * 1).skip((page - 1) * limit)
+        console.log(sorting)
+        const products = await Product.find(query).sort(sorting).limit(limit * 1).skip((page - 1) * limit)
         res.status(200).send({ data: products });
     } catch (error) {
         res.status(400).send(error.message);
